697cd87fd43777d68d392f55ed76a1f332380ba1
Josimar Minete <josimaminete@gmail.com>
2022-06-28 08:47:57 -0300

    Revisando algumas configurações do web.xml

--- src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,9 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// A ANOTAÇÃO FOI COMENTADA POIS ESTA CONFIGURADO NO ARQUIVO XML
 //oi
-@WebServlet(urlPatterns = "/oi")
+//@WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet {
+
+	// NÃO TEM MAIN POIS QUEM DA NEW É O SERVLETCONTAINER O TONCAT NO CASO
+	// TONCAT MIDDLEWARE É UM INTERMEDIÁRIO ENTRE A REUISIÇÃO HTTP E A NOSSA CLASSE.
+	public OiMundoServlet() {
+		// O TOMCAT QUE CRIA ESTE CONSTRUTOR
+		// O TONCAR SÓ CRIA A INSTANCIA QUANDO ALGUEM CHAMA
+		// SERVLET É UM SINGLETON POIS SÓ TEM UMA INSTANCIA DESSA CLASSE AQUI
+		// SERVLET VIVE PARA SEMPRE SÓ UMA INSTANCIA PARA TODAS REQUISIÇÕES HTTP
+		System.out.println("Criando OiMundoServlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
