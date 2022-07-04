9694e11c4ff6911ac49b5cab45cf63a1c1b0e47e
Josimar Minete <josimaminete@gmail.com>
2022-07-04 09:35:17 -0300

    Adicionado interceptador.

--- src/br/com/alura/gerenciador/acao/ListarEmpresas.java
+++ src/br/com/alura/gerenciador/acao/ListarEmpresas.java
@@ -13,14 +13,9 @@ import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 //Esta class não é um servlet e serve para passar por dentro do interceptador.
-public class ListarEmpresas implements Acao{
+public class ListarEmpresas implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
 		
 		System.out.println("---------ListarEmpresas");
 		Banco banco = new Banco();
